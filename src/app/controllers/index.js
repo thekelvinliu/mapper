d051bd3715ecd2e7c559acbc91a9893f1f401859
thekelvinliu <kelvin@thekelvinliu.com>
2016-05-20 22:04:57 -0500

    added more routes

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -1,33 +1,41 @@
 'use strict';
-// this controller is meant to set up routes from all other controllers
-// it also sets up basic express routes
 
 import express from 'express';
-import mongoose from 'mongoose';
-
-// load models
-const Movie = mongoose.model('Movie');
 
 // create router
 const router = express.Router();
-// load other controllers
-router.use('/extras', require('./extras'));
-
-// set basic routes
-router.get('/', (req, res, next) => {
-  res.render('index', {
-    title: 'mapper'
-  });
-});
-router.get('/movies', (req, res, next) => {
-  Movie.find((err, movies) => {
-    if (err) return next(err);
-    res.render('movies', {
-      title: 'Movies!',
-      movies
-    });
-  });
-});
+// main page
+router.get('/', (req, res, next) => res.render('index', {
+  title: 'mapper',
+  users: [
+    'KLIU78',
+    'HAPPYFLOPP',
+    '123456789012345',
+    '12345678901234567890'
+  ]
+}));
+// about page
+router.get('/about', (req, res, next) => res.render('about', {
+  title: '*about*'
+}));
+// register page
+router.get('/register', (req, res, next) => res.render('register', {
+  title: '*register*'
+}));
+// user page
+router.get('/users/:user', (req, res, next) => res.render('users', {
+  title: `${req.params.user}`,
+  data: [{
+    lat: 0,
+    lng: 0
+  }, {
+    lat: 1,
+    lng: 1
+  }, {
+    lat: 2,
+    lng: 2
+  }]
+}));
 
 // export router
 module.exports = router;
