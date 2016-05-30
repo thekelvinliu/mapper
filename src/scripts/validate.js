d644595d5786ca02723241883b24fd56ed23e7bf
thekelvinliu <kelvin@thekelvinliu.com>
2016-05-30 15:27:39 -0500

    use inline script for input validation

--- src/scripts/validate.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// const validate = () => /^[a-zA-Z0-9]+$/.test(document.forms['register']['user'].value);
-
-function validate() {
-  return /^[a-zA-Z0-9]+$/.test(document.forms['register']['user'].value);
-}
